3a10e705f4f52d05f8b51b8c429b0149575a1d63
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-27 02:48:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,20 @@
 class Dog 
+  attr_accessor :name
   
   @@all = []
   
-  def initialize
+  def initialize(name)
    @@all.push(self)
+   @name = name 
   end 
   
   def self.all 
+    return @@all
+  end 
+  
+  def self.print_all
+    @@all.each do |dog|
+      puts @name 
+    end
+  end
+end 
